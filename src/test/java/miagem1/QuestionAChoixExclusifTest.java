de0803ce2e7c1b142302724532a4520f157e250e
TovarichMFS <calmels.dimitri@gmail.com>
2019-11-22 17:07:44 +0100

    fix #1.1 maj Tests sur QuestionAChoixExclusif

--- src/test/java/miagem1/QuestionAChoixExclusifTest.java
+++ src/test/java/miagem1/QuestionAChoixExclusifTest.java
@@ -2,20 +2,27 @@ package miagem1;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class QuestionAChoixExclusifTest {
+    private QuestionAChoixExclusif q;
+
+    @Before
+    public void setUp() throws Exception{
+        this.q = new QuestionAChoixExclusif("LOL?",3);
+
+    }
 
     @Test
     public void testGetEnonce() {
-        QuestionAChoixExclusif q = new QuestionAChoixExclusif("LOL?",3);
         assertEquals("L'énoncé ne correspond pas!","LOL?",q.getEnonce());
     }
 
     @Test
     public void testGetScoreForIndice() {
-        QuestionAChoixExclusif q = new QuestionAChoixExclusif("LOL?",3);
         assertEquals("Le score ne correspond pas!",100,q.getScoreForIndice(3),0.0);
-        assertThat("Le score ne correspond pas!",100.0f,not(equalTo(q.getScoreForIndice(1))));
+        assertThat("Le score ne devrait pas correspondre!",100.0f,not(equalTo(q.getScoreForIndice(1))));
     }
 }
