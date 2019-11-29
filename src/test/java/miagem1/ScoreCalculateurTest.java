a756e14fbe68d1d4cbd056385db3471b2bd5ac09
TovarichMFS <calmels.dimitri@gmail.com>
2019-11-29 14:08:25 +0100

    fix #3 Tests sur ScoreCalculateur

--- src/test/java/miagem1/ScoreCalculateurTest.java
+++ src/test/java/miagem1/ScoreCalculateurTest.java
@@ -10,22 +10,29 @@ import static org.junit.Assert.*;
 
 public class ScoreCalculateurTest {
     private ScoreCalculateur sc;
-    private QuestionAChoixExclusif qE;
     private QuestionAChoixMultiple qM;
 
     @Before
     public void setUp() throws Exception {
         this.sc = new ScoreCalculateur();
-        this.qE = new QuestionAChoixExclusif("LOL?",2);
-        this.qM = new QuestionAChoixMultiple("q1",new ArrayList<Integer>(Arrays.asList(2,3,5)));
+        qM = new QuestionAChoixMultiple("q1",new ArrayList<Integer>(Arrays.asList(2,3,5)));
     }
 
     @Test
-    public void calculeScoreExclusif() {
-        float score = 100;
+    public void testCalculeScore() {
         ArrayList<Integer> reponses = new ArrayList<Integer>();
         reponses.add(1);
-        reponses.add(9);
-        assertEquals("Le score ne correspond pas!",score,sc.calculeScore(reponses,qE),0.0);
+        reponses.add(4);
+        assertEquals("1: Le score 0 ne correspond pas!",0,sc.calculeScore(reponses,qM),0.0);
+        reponses = new ArrayList<Integer>();
+        reponses.add(2);
+        reponses.add(3);
+        float scorePartiel = 200f/3;
+        assertEquals("2: Le score pour réponses partielles ne correspond pas!",scorePartiel,sc.calculeScore(reponses,qM),0.01);
+        reponses = new ArrayList<Integer>();
+        reponses.add(2);
+        reponses.add(3);
+        reponses.add(5);
+        assertEquals("3: Le score maximal ne correspond pas!",100,sc.calculeScore(reponses,qM),0.01);
     }
 }
